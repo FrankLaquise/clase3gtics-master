94b161ac85450d27f319ad2f90af9c2a767d4b16
Sergio <sergio.sotomayor@pucp.edu.pe>
2022-09-06 16:04:11 -0500

    controller primera version

--- src/main/java/com/example/clase3gtics/controller/ShipperController.java
+++ src/main/java/com/example/clase3gtics/controller/ShipperController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
-
+// frank puta
 @Controller
 @RequestMapping("/shipper")
 public class ShipperController {
